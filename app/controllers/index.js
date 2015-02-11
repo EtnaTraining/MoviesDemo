ddc09c1a6c37a2cc6c2693171bb35e44e7d539bd
Antonio Calanducci <calanducci@etnatraining.it>
2015-02-11 22:50:10 +0100

    adding search

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,31 +1,45 @@
-function transformModel(model) {
-	var attrs = model.attributes;
-	attrs.score = attrs.ratings.critics_score;
-	attrs.thumbnail = attrs.posters.thumbnail.replace("_tmb", "_pro");
-	return attrs;
-}
-if (OS_IOS) {
-	var navWin = Ti.UI.iOS.createNavigationWindow({
-		window: $.index
-	});
-	navWin.open();
-} else {
-	$.index.open();
 
+
+if (OS_IOS) {
+    var navWin = Ti.UI.iOS.createNavigationWindow({
+        window: $.index
+    });
+    navWin.open();
+    $.list.filterAttribute = "name";
+} else {
+    $.index.open();
+    $.list.filterAttribute = "title";
 }
+
+
+var net = require('net');
+$.loading.show();
+net.getMovies(function(movies) {
+    $.loading.hide();
+    Alloy.Collections.movieList.reset(movies);
+});
+
+
+
 
 function showDetail(e) {
-	var detailWin = Alloy.createController("detail", e.index).getView();
-	if (OS_IOS) {
-		navWin.openWindow(detailWin);
-	} else {
-		detailWin.open();
-	}
-	
-	
+    var detailWin = Alloy.createController("detail", e.index).getView();
+    if (OS_IOS) {
+        navWin.openWindow(detailWin);
+    } else {
+        detailWin.open();
+    }
 }
 
-Alloy.Globals.loading.show();
+function transformModel(model) {
+    var attrs = model.attributes;
+    attrs.score = attrs.ratings.critics_score;
+    attrs.thumbnail = attrs.posters.thumbnail.replace("_tmb", "_pro");
+    return attrs;
+}
+
+
+
 /*
 setTimeout(function() {
 	showDetail({index: 0});
--- app/controllers/movie_cell.js
+++ app/controllers/movie_cell.js
@@ -1,9 +1,11 @@
 
 
 var score = $model.get("ratings").critics_score;
-
 $.score.color = Alloy.Globals.setColor(score);
-
 $.score.text += "%";
 
-
+if (OS_IOS) {
+	$.movie_cell.name = $.title.text;
+} else {
+	$.movie_cell.title = $.title.text;
+}
